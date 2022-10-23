c089f75bfdd9af5d6027be73b47f62762833ae0f
84DuLu <knight36999@163.com>
2022-10-23 19:16:29 +0800

    Add function playRound()

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let numberForChoice = getRndInteger(0, 2);
-    
+
     if (numberForChoice === 0) {
         return "rock";
     } else if (numberForChoice === 1) {
@@ -22,6 +25,24 @@ function getPlayerChoice() {
     } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
     
     return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let computerChoice = computerSelection();
+    let playerChoice = playerSelection();
+
+    if (computerChoice === playerChoice) {
+        console.log(`Your ${playerChoice} ties with ${computerChoice}`);
+        return "both";
+    } else if (playerChoice === "scissors" && computerChoice === "paper" || 
+               playerChoice === "rock" && computerChoice === "scissors"  ||
+               playerChoice === "paper" && computerChoice === "rock") {
+        console.log(`Your ${playerChoice} beats ${computerChoice}`);
+        return "player";
+    } else {
+        console.log(`Your ${playerChoice} is beaten by ${computerChoice}`);
+        return "computer";
+    }
 }
 
 function getRndInteger(min, max) {
