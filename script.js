6fef33fe3746c8d7fec4986669681fd2ca75e007
84DuLu <knight36999@163.com>
2022-10-27 16:04:24 +0800

    Add UI

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+let computerChoice;
+let winner;
+
+const startButton = document.querySelector('.start-button');
+const buttons = document.querySelectorAll('.button');
+const playerScoreBoard = document.querySelector('#player-score');
+const computerScoreBoard = document.querySelector('#computer-score');
+const info = document.querySelector('.info');
+const rock = document.querySelector('#rock');
+const scissors = document.querySelector('#scissors');
+const paper = document.querySelector('#paper');
+const clickSound = document.querySelector('#click-sound');
+
 function getComputerChoice() {
     let numberForChoice = getRndInteger(0, 2);
 
@@ -24,20 +40,17 @@ function getPlayerChoice() {
     return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let computerChoice = computerSelection();
-    let playerChoice = playerSelection();
-
+function playRound(playerChoice, computerChoice) {
     if (computerChoice === playerChoice) {
-        console.log(`Your ${playerChoice} ties with ${computerChoice}`);
+        info.textContent = `Your ${playerChoice} ties with ${computerChoice}`;
         return "both";
     } else if (playerChoice === "scissors" && computerChoice === "paper" || 
                playerChoice === "rock" && computerChoice === "scissors"  ||
                playerChoice === "paper" && computerChoice === "rock") {
-        console.log(`Your ${playerChoice} beats ${computerChoice}`);
+        info.textContent = `Your ${playerChoice} beats ${computerChoice}`;
         return "player";
     } else {
-        console.log(`Your ${playerChoice} is beaten by ${computerChoice}`);
+        info.textContent = `Your ${playerChoice} is beaten by ${computerChoice}`;
         return "computer";
     }
 }
@@ -70,4 +83,53 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-game();
+function buttonClick(e) {
+    clickSound.play();
+
+    playerChoice = e.target.id;
+    computerChoice = getComputerChoice();
+    winner = playRound(playerChoice, computerChoice);
+    if (winner === "player") {
+        playerScore += 1;   
+    } else if (winner === "computer") {
+        computerScore += 1;
+    }
+    playerScoreBoard.textContent = playerScore;
+    computerScoreBoard.textContent = computerScore;
+
+    buttons.forEach((item) => item.style.background = '#9CA3AF');
+    if (winner === 'both') {
+        e.target.style.background = 'linear-gradient(to right, #F78F08 50%, #0870F7 50%)';
+    } else{
+        e.target.style.background = '#F78F08';
+        buttons.forEach((item) => {
+            if (item.id === computerChoice) item.style.background = '#0870F7';
+        });
+    }
+
+    if (playerScore === 5 || computerScore === 5) {
+        buttons.forEach((item) => item.removeEventListener('click', buttonClick));
+        if (playerScore === 5) {   
+            info.textContent = 'PLAYER WINS!';
+            document.querySelector('#win-sound').play();
+        } else {
+            info.textContent = 'COMPUTER WINS!';
+            document.querySelector('#lose-sound').play();
+        }
+    }
+}
+
+startButton.addEventListener('click', () => {
+    clickSound.play();
+    playerScore = 0;
+    computerScore = 0;
+
+    playerScoreBoard.textContent = playerScore;
+    computerScoreBoard.textContent = computerScore;
+    buttons.forEach((item) => item.style.background = '#9CA3AF');
+    info.textContent = 'First one to get five points wins!';
+
+    buttons.forEach((item) => {
+        item.addEventListener('click', buttonClick);
+    });
+});
